d9ca94d2a7fcedbc254307ff345a2981ac985898
xfding <xfding@vw-mobvoi.com>
2021-01-29 11:25:22 +0800

    add setup script

---
+++ setup.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+work_path=$(dirname $(readlink -f $0))
+
+apt-get update && apt-get install -y  \
+    procps \
+    autoconf \
+    automake \
+    bzip2 \
+    g++ \
+    git \
+    gstreamer1.0-plugins-good \
+    gstreamer1.0-tools \
+    gstreamer1.0-pulseaudio \
+    gstreamer1.0-plugins-bad \
+    gstreamer1.0-plugins-base \
+    gstreamer1.0-plugins-ugly  \
+    libatlas3-base \
+    libgstreamer1.0-dev \
+    libtool-bin \
+    make \
+    python3 \
+    python-pip \
+    python-yaml \
+    python-simplejson \
+    python-gi \
+    subversion \
+    unzip \
+    wget \
+    build-essential \
+    python-dev \
+    sox \
+    zlib1g-dev && \
+    apt-get clean autoclean && \
+    apt-get autoremove -y && \
+    pip install ws4py==0.3.2 && \
+    pip install tornado
+
+cd ~
+
+wget http://www.digip.org/jansson/releases/jansson-2.7.tar.bz2 && \
+    bunzip2 -c jansson-2.7.tar.bz2 | tar xf -  && \
+    cd $work_path/jansson-2.7 && \
+    ./configure && make -j $(nproc) && make check &&  make install && \
+    echo "/usr/local/lib" >> /etc/ld.so.conf.d/jansson.conf && ldconfig && \
+    rm $work_path/jansson-2.7.tar.bz2 && rm -rf $work_path/jansson-2.7
+
+cd $work_path
+
+git clone https://github.com/kaldi-asr/kaldi && \
+    cd $work_path/kaldi/tools && \
+    make -j $(nproc) && \
+    ./install_portaudio.sh && \
+    $work_path/kaldi/tools/extras/install_mkl.sh && \
+    cd $work_path/kaldi/src && ./configure --shared && \
+    sed -i '/-g # -O0 -DKALDI_PARANOID/c\-O3 -DNDEBUG' kaldi.mk && \
+    make clean -j $(nproc) && make -j $(nproc) depend && make -j $(nproc) && \
+    cd $work_path/kaldi/src/online && make depend -j $(nproc) && make -j $(nproc) && \
+    cd $work_path/kaldi/src/gst-plugin && sed -i 's/-lmkl_p4n//g' Makefile && make depend -j $(nproc) && make -j $(nproc) && \
+    cd $work_path/gst-kaldi-nnet2-online/src && \
+    sed -i '/KALDI_ROOT?=\/home\/tanel\/tools\/kaldi-trunk/c\KALDI_ROOT?=$work_path\/kaldi' Makefile && \
+    make depend -j $(nproc) && make -j $(nproc) && \
+    find $work_path/gst-kaldi-nnet2-online/src/ -type f -not -name '*.so' -delete && \
+    rm -rf $work_path/kaldi/.git && \
+    rm -rf $work_path/kaldi/egs/ $work_path/kaldi/windows/ $work_path/kaldi/misc/ && \
+    find $work_path/kaldi/src/ -type f -not -name '*.so' -delete && \
+    find $work_path/kaldi/tools/ -type f \( -not -name '*.so' -and -not -name '*.so*' \) -delete && \
+    cd $work_path
+
